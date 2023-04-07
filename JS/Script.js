2d98bccb0c94edbcad4e1a084a99891756cb4f8a
xYANESUKYx <93495032+xYANESUKYx@users.noreply.github.com>
2023-04-07 21:27:16 +0100

    Add files via upload

---
+++ JS/Script.js
@@ -0,0 +1,46 @@
+
+var contador = 0; 
+
+function TocarNota(nota){
+    contador = 0;
+
+    let audioNota = document.getElementById(nota);
+    audioNota.play();
+
+    var notasGif = document.getElementById("notas");
+    var gotasGif = document.getElementById("gotas");
+    var Sonrojo = document.getElementById("Sonrojo");
+    var nombreNota = document.getElementById("nombreNota");
+
+    notasGif.style.display = "none";
+    gotasGif.style.display = "none";
+    Sonrojo.style.display = "none";
+    nombreNota.textContent = " ";
+    nombreNota.style.border = "";
+
+    notasGif.style.display = "block";
+    gotasGif.style.display = "block";
+    Sonrojo.style.display = "block";
+    nombreNota.textContent = nota;
+    nombreNota.style.border = "solid";
+}
+
+function contando(){
+    contador++;
+
+    if(contador == 4){
+        var notasGif = document.getElementById("notas");
+        var gotasGif = document.getElementById("gotas");
+        var Sonrojo = document.getElementById("Sonrojo");
+        var nombreNota = document.getElementById("nombreNota");
+
+        notasGif.style.display = "none";
+        gotasGif.style.display = "none";
+        Sonrojo.style.display = "none";
+        nombreNota.textContent = " ";
+        nombreNota.style.border = "";
+    }
+} 
+
+var cont = setInterval('contando()', 1000);
+
